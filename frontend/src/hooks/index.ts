1c7a93fc9572a05020dca1327b83b2199cd687a0
Iszyeffiong <israel.iszy2336@gmail.com>
2026-06-16 09:43:43 +0100

    Task 38: Create custom hook useFundTask to handle escrow deposits

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from './useCreateTask';
+export * from './useAcceptTask';
+export * from './useSubmitTask';
+export * from './useApproveTask';
+export * from './useGetTasks';
+export * from './useFetchTaskById';
+export * from './useUserReputation';
